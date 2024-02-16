1d080eb2d3567cbe98d08ed02b1fd9faf6545f8c
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-17 01:39:08 +0530

    coments removed from locations

--- src/functions/locations.js
+++ src/functions/locations.js
@@ -2,34 +2,20 @@ const { Child } = require("../database/models");
 const distance = require("../functions/distance");
 
 const updateLastLocation = async (loc, childId) => {
-  console.log("ChildID");
-  console.log(childId);
-  console.log("loc in up");
-  console.log(loc);
   try {
-    console.log(loc + " " + childId);
     const child = await Child.findOne({ id: childId });
     if (child) {
-      console.log("Bef");
-      console.log(child);
       child.lastLocation = loc;
-      console.log("Af");
       if (child.locHistory.length === 0) {
-        console.log("In if");
         child.locHistory.push({
           time: new Date(),
           loc: loc,
         });
       } else {
-        console.log("In else");
         const topLoc = child.locHistory[child.locHistory.length - 1];
-        console.log("top loc");
-        console.log(topLoc.loc);
-        console.log("loc");
-        console.log(loc);
         const dist = distance(topLoc.loc, loc);
         console.log(dist);
-        if (dist >= 100) {
+        if (dist >= 500) {
           child.locHistory.push({
             time: new Date(),
             loc: loc,
