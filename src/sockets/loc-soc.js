01ccc24d83ec9ca50d8b396c42c90670f171d4e4
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-16 23:14:19 +0530

    new demo

--- src/sockets/loc-soc.js
+++ src/sockets/loc-soc.js
@@ -12,7 +12,9 @@ function locationSocket(io, socket) {
     console.log("rec location");
     const location = { lat: loc.coords.latitude, lon: loc.coords.longitude };
     updateLastLocation(location, socket.handshake.query.childId);
+    console.log("loc in soc");
     console.log(location);
+    console.log(socket.handshake.query.childId);
     updateLoc(socket.handshake.query.childId, location);
   });
 
