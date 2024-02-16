d7dad8923a75b997477524d58c45feb93fe83c94
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-16 21:12:05 +0530

    parent  socket added

--- src/sockets/parent-soc.js
+++ src/sockets/parent-soc.js
@@ -1,5 +1,15 @@
-function locationSocket(io, socket) {
-    socket.on('get-child-live', (id) => {
-        
-    })
-  }
+const { Child } = require("../database/models.js");
+
+function parentSocket(io, socket) {
+  socket.on("get-child-live", async (id) => {
+    socket.emit("child-live-loc", (id, loc));
+    try {
+      const child = await Child.findOne({ id: id });
+      socket.emit("child-live-loc", (id, child.lastLocation));
+    } catch (err) {
+      console.log(err);
+    }
+  });
+}
+
+module.exports = parentSocket;
--- src/sockets/socket-main.js
+++ src/sockets/socket-main.js
@@ -1,10 +1,12 @@
 const locationSocket = require("./loc-soc");
+const parentSocket = require("../sockets/parent-soc");
 
 const setupSocketIO = (io) => {
   socketIo = io;
   io.on("connection", (socket) => {
     try {
       if (socket.handshake.query.isChild) locationSocket(io, socket);
+      else parentSocket(io, socket);
       console.log("connected to socket");
     } catch (err) {
       console.log(err);
