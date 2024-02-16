57a041bfa38851acc532961c1afd544e5576fbfd
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-16 20:53:41 +0530

    hope error done test prt-1

--- src/sockets/loc-soc.js
+++ src/sockets/loc-soc.js
@@ -1,17 +1,12 @@
-const { updateLastLocation } = require("../functions/locations");
+const updateLastLocation = require("../functions/locations");
 
 function locationSocket(io, socket) {
-  try {
-    socket.on("locLive", (loc) => {
-      console.log("rec location");
-      console.log(loc);
-      const location = { lat: loc.latitude, lon: loc.longitude };
-      updateLastLocation(location, socket.handshake.query.childId);
-    });
-  } catch (err) {
-    console.log("Error occured");
-    console.log(err);
-  }
+  socket.on("locLive", (loc) => {
+    console.log("rec location");
+    console.log(loc);
+    const location = { lat: loc.latitude, lon: loc.longitude };
+    updateLastLocation(location, socket.handshake.query.childId);
+  });
 }
 
 module.exports = locationSocket;
