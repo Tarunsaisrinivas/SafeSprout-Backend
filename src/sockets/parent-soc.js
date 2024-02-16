6e3527d5923f58268c36816fc435f5e01b0b95be
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-17 02:07:37 +0530

    lknjg

--- src/sockets/parent-soc.js
+++ src/sockets/parent-soc.js
@@ -1,20 +1,15 @@
-const { Child } = require("../database/models.js");
+const { Child, User } = require("../database/models.js");
 const { getMap } = require("../utilities/childMap");
 
 function parentSocket(io, socket) {
-  console.log("in parent socket");
-  socket.on("get-child-live", async (id) => {
-    console.log("get socket call");
-    console.log(id);
-    try {
-      const map = getMap();
-      console.log(id);
-      console.log(map);
-      socket.emit("child-live-loc", (id, map.get(id)));
-    } catch (err) {
-      console.log(err);
-    }
-  });
+  const email = socket.handshake.query.email;
+  setInterval(async () => {
+    const usr = await User.findOne({ email: email });
+    usr.children.forEach((id) => {
+      const loc = getMap().get(id);
+      socket.emit("child-loc-live", { stat: true, loc: loc, id: id });
+    });
+  }, 5000);
 }
 
 module.exports = parentSocket;
